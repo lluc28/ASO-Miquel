4e6cdb7ea957b623538dd67b771d108e0eaa16ab
lluc28 <lcrivilles391@boscdelacoma.cat>
2024-10-08 17:51:35 +0200

    17:50

--- percentatgeCPU.sh
+++ percentatgeCPU.sh
@@ -2,5 +2,7 @@
 
 # Aquesta és l'operació que volem fer (100*cputime)/(etime) que és igual al percentatge ús de la cpu
 PID=$1
-DADES=$(ps -p $PID pid=,cputime=cputime,etime=etime)
- 
+DADES=$(ps -p $PID -o cputime=,etime=)
+
+CPUTIME = cut -d 1
+ETIME = cut -d 2
