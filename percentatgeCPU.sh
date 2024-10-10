5999529f81f7f4be6ae4663bbda126b835af93f2
lluc28 <lcrivilles391@boscdelacoma.cat>
2024-10-10 20:03:01 +0200

    10/10

--- percentatgeCPU.sh
+++ percentatgeCPU.sh
@@ -2,7 +2,15 @@
 
 # Aquesta és l'operació que volem fer (100*cputime)/(etime) que és igual al percentatge ús de la cpu
 PID=$1
+
 DADES=$(ps -p $PID -o cputime=,etime=)
 
-CPUTIME = cut -d 1
-ETIME = cut -d 2
+CPUTIME=$(echo "$DADES" | cut -d ' ' -f 1)
+ETIME=$(echo "$DADES" | cut -d ' ' -f 2)
+
+CPUTIME_SEC=$(echo $CPUTIME | cut -d ':' --output-delimiter=' ' -f 1,2,3 | { read h m s; echo $((10#$h*3600 + 10#$m*60 + 10#$s)); })
+ETIME_SEC=$(echo $ETIME | cut -d ':' --output-delimiter=' ' -f 1,2,3 | { read h m s; echo $((10#$h*3600 + 10#$m*60 + 10#$s)); })
+
+CPU_PERCENT=$(( 100 * CPUTIME_SEC / ETIME_SEC ))
+
+echo "$CPU_PERCENT% de CPU utilitzat pel procés amb PID $PID"
