a4b3032dd3de3eade1a85d94a8e8cf2d5e4eb750
lluc28 <lcrivilles391@boscdelacoma.cat>
2024-11-19 17:51:37 +0100

    19/11 script creacio grups

--- Documentació/AWS/AWSCLI_Practica/grupsdeseguretat.sh
+++ Documentació/AWS/AWSCLI_Practica/grupsdeseguretat.sh
@@ -1,6 +1,34 @@
-# crear grup de seguretat
-# agafar la id del no grup
-# crera les variables per crear el windows
-# crear el windows amb les variables
+#!/bin/sh
 
+# Crear grup de seguretat
+aws ec2 create-security-group --group-name lluc-sg --description "Grup de Seguretat per a Hackathon"
+
+# Guardar ID del grup de seguretat, ho faig per després assignar aquesta ID a la màquina
+ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values="lluc-sg" --query 'SecurityGroups[0].[GroupId]' --output text)
+
+# Comprovar si hi ha hagut algun error al crear el grup perquè sino no funcionara res
+if [ $? -ne 0 ]; then
+    echo "Error creant el grup de seguretat"
+    exit 2
+fi
+
+#Aquí he obert els ports  necessaris per tota la infraestructura
+# SSH
+aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 22 --cidr 0.0.0.0/0
+# RDP
+aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 3389 --cidr 0.0.0.0/0
+# HTTP
+aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 80 --cidr 0.0.0.0/0
+# HTTPS
+aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 443 --cidr 0.0.0.0/0
+# DNS (UDP)
+aws ec2 authorize-security-group-ingress --group-id $ID --protocol udp --port 53 --cidr 0.0.0.0/0
+# DNS (TCP)
+aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 53 --cidr 0.0.0.0/0
+# LDAP
+aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 389 --cidr 0.0.0.0/0
+# Secure LDAP
+aws ec2 authorize-security-group-ingress --group-id $ID --protocol tcp --port 636 --cidr 0.0.0.0/0
+
+echo "Grup de seguretat creat amb ID: $ID"
 
