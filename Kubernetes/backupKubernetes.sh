8bff80505972622c8d9bfcd73cdbafd652d2508f
lluc28 <lcrivilles391@boscdelacoma.cat>
2025-03-07 15:47:47 +0100

    Retocs finals 7/03/2025

--- Kubernetes/backupKubernetes.sh
+++ Kubernetes/backupKubernetes.sh
@@ -12,22 +12,41 @@ FITXER="backup-$DATA.tar.gz"
 
 # Comprovar directoris locals abans de fer el backup si falta algun d'aquests directoris acaba el programa
 echo "Comprovant carpetes..."
-[ -d /var/lib/etcd ] || { echo "Error: No existeix /var/lib/etcd"; exit 1; }
-[ -d /etc/kubernetes ] || { echo "Error: No existeix /etc/kubernetes"; exit 1; }
-[ -d /home/asix/odoo-kubernetes ] || { echo "Error: No existeix /home/asix/odoo-kubernetes"; exit 1; }
+if [ ! -d /var/lib/etcd ]; then
+    echo "Error: No existeix /var/lib/etcd"
+    exit 1
+fi
+
+if [ ! -d /etc/kubernetes ]; then
+    echo "Error: No existeix /etc/kubernetes"
+    exit 1
+fi
+
+if [ ! -d /home/asix/odoo-kubernetes ]; then
+    echo "Error: No existeix /home/asix/odoo-kubernetes"
+    exit 1
+fi
 
 # Ara creo l'arxiu de backup i el comprimeixo amb les carpetes necessaries
-echo "Comprimint..."
-sudo tar -czf $FITXER /var/lib/etcd /etc/kubernetes /home/asix/odoo-kubernetes
+sudo tar -czf "$FITXER" /var/lib/etcd /etc/kubernetes /home/asix/odoo-kubernetes
+if [ $? -ne 0 ]; then
+    echo "ERROR: La compressió ha fallat (codi d'error: $?)"
+    exit 1
+fi
 
 # Transfereixo el fitxer comprimit i l'envio amb scp a on haguem posat a la part de dalt del programa
-echo "Enviant..."
-scp -i $CLAU_SSH $FITXER $USUARI_REMOT@$DESTI_IP:$DIRECTORI_REMOT
+scp -i "$CLAU_SSH" "$FITXER" "$USUARI_REMOT@$DESTI_IP:$DIRECTORI_REMOT"
+if [ $? -ne 0 ]; then
+    echo "ERROR: Fallida en el transferiment a $DESTI_IP (codi d'error: $?)"
+    exit 1
+fi
 
 # FInalment elimino els arxius locals de backup un cop la transferencia esta feta aixi no n'acumulem
-echo "Esborrant temporal..."
-rm $FITXER
-
-# COnfirmació final amb la ruta del backup remot
-echo "Tot correcte! Backup guardat a:"
-echo "$USUARI_REMOT@$DESTI_IP:$DIRECTORI_REMOT$FITXER"
+rm "$FITXER"
+if [ $? -ne 0 ]; then
+    # En aquest cas falla l'esborrat
+    echo "El Backup s'ha enviat correctament, però hi ha hagut un error al borrar-ho localment (codi $?)"
+else
+    # Aqui ha funcionat tot
+    echo "Finalmnet el Backup s'ha executat correctament i s'han fet totes les operacions amb èxit"
+fi
